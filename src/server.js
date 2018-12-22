85bb98b081f4929b91bcf541dd6f3a7e8a3af659
Henri-Marcel Hopkins <clemlaf43@hotmail.fr>
2018-12-22 23:30:17 +0100

    deuxième commit

--- src/server.js
+++ src/server.js
@@ -474,7 +474,7 @@ models.sequelize.sync().then(function() {
    * 
    * Listen only when database connection is sucessfull
    */
-  app.listen(3000, function() {
+  app.listen(process.env.PORT, function() {
     console.log('Express server listening on port !');
   });
 });
